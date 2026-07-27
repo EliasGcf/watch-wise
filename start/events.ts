e67378d9aa5a6b25ca79f496586610cf93756321
Elias Gabriel <eliasgabrielcf@gmail.com>
2026-07-27 18:06:47 -0300

    refactor: register multiple listeners at once

--- start/events.ts
+++ start/events.ts
@@ -1,11 +1,17 @@
 import emitter from '@adonisjs/core/services/emitter'
 import { events } from '#generated/events'
 import { listeners } from '#generated/listeners'
-const updateSeriesProgress = () => import('#listeners/update_series_progress')
 
-emitter.on(events.MovieWatched, [listeners.UpdateUserWatchedTime])
-emitter.on(events.MovieUnwatched, [listeners.UpdateUserWatchedTime])
-emitter.on(events.EpisodeWatched, [listeners.UpdateUserWatchedTime])
-emitter.on(events.EpisodeUnwatched, [listeners.UpdateUserWatchedTime])
-emitter.on(events.EpisodeWatched, [updateSeriesProgress])
-emitter.on(events.EpisodeUnwatched, [updateSeriesProgress])
+emitter.listen(events.MovieWatched, [listeners.UpdateUserWatchedTime])
+
+emitter.listen(events.MovieUnwatched, [listeners.UpdateUserWatchedTime])
+
+emitter.listen(events.EpisodeWatched, [
+  listeners.UpdateUserWatchedTime,
+  listeners.UpdateSeriesProgress,
+])
+
+emitter.listen(events.EpisodeUnwatched, [
+  listeners.UpdateUserWatchedTime,
+  listeners.UpdateSeriesProgress,
+])
