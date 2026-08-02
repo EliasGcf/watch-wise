8a526bc10a01aeef5e837798b4ef418d3d9758c1
Elias Gabriel <eliasgabrielcf@gmail.com>
2026-08-02 11:43:50 -0300

    docs: add note on editing the .adonisjs folder

--- .adonisjs/server/controllers.ts
+++ .adonisjs/server/controllers.ts
@@ -5,12 +5,12 @@
 
 export const controllers = {
   api: {
+    Library: () => import('#controllers/api/library_controller'),
     Movies: () => import('#controllers/api/movies_controller'),
     series: {
       Episodes: () => import('#controllers/api/series/episodes_controller'),
       Seasons: () => import('#controllers/api/series/seasons_controller'),
     },
-    Library: () => import('#controllers/api/library_controller'),
   },
   web: {
     CatalogSearch: () => import('#controllers/web/catalog_search_controller'),
