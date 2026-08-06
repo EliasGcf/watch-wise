8520ecad200f835575766fb2995fd749142df545
Elias Gabriel <eliasgabrielcf@gmail.com>
2026-08-06 13:19:00 -0300

    refactor: simplify sonarr provider manager

--- providers/sonarr/manager.ts
+++ providers/sonarr/manager.ts
@@ -14,8 +14,8 @@ export class SonarrProviderManager extends SonarrProvider {
     super()
   }
 
-  use(name: SonarrDriver = this.enabledDriver()) {
-    if (name === 'fake') return this.createDriver(name)
+  use(name: SonarrDriver | undefined = this._config.default) {
+    if (!name) throw new SonarrProviderError('Sonarr provider is not configured.')
 
     const cachedDriver = this.#drivers.get(name)
 
@@ -25,20 +25,6 @@ export class SonarrProviderManager extends SonarrProvider {
     this.#drivers.set(name, driver)
 
     return driver
-  }
-
-  config<T extends SonarrDriver>(
-    driver: T = this.enabledDriver() as T
-  ): SonarrProviderConfig['drivers'][T] {
-    const config = this._config.drivers[driver]
-    if (!config) throw new SonarrProviderError(`Unsupported Sonarr provider driver "${driver}"`)
-    return config
-  }
-
-  private enabledDriver() {
-    if (!this._config.default) throw new SonarrProviderError('Sonarr provider is not configured.')
-
-    return this._config.default
   }
 
   private createDriver(name: SonarrDriver) {
