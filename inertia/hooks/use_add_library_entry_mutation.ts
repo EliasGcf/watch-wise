c749c64d2ff5133fbe91de772cbec1eb12c526df
Elias Gabriel <eliasgabrielcf@gmail.com>
2026-08-08 16:56:37 -0300

    fix: reload catalog page after adding a title to library

--- inertia/hooks/use_add_library_entry_mutation.ts
+++ inertia/hooks/use_add_library_entry_mutation.ts
@@ -1,3 +1,4 @@
+import { router } from '@inertiajs/react'
 import { useMutation } from '@tanstack/react-query'
 import { toast } from 'sonner'
 import { api } from '~/client'
@@ -5,7 +6,10 @@ import { api } from '~/client'
 export function useAddLibraryEntryMutation() {
   return useMutation(
     api.api.library.store.mutationOptions({
-      onSuccess: () => toast.success('Title was added to your library.'),
+      onSuccess: async () => {
+        toast.success('Title was added to your library.')
+        await new Promise<void>((resolve) => router.reload({ onFinish: () => resolve() }))
+      },
       onError: () => toast.error('Title could not be added to your library.'),
     })
   )
