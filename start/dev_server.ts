9cb0ce75b62fc999c67ae5ef5d405cb4c3a3cce4
Elias Gabriel <eliasgabrielcf@gmail.com>
2026-08-09 22:34:07 -0300

    chore: format

--- start/dev_server.ts
+++ start/dev_server.ts
@@ -3,7 +3,7 @@ import { hooks } from '@adonisjs/core/app'
 const cyan = (text: string) => `\x1b[36m${text}\x1b[0m`
 
 export default hooks.devServerStarted((_, info, instructions) => {
-  const baseUrl = process.env.PORTLESS_URL ?? process.env.APP_URL;
+  const baseUrl = process.env.PORTLESS_URL ?? process.env.APP_URL
   instructions.add(`APP: ${cyan(`${baseUrl}/app`)}`)
   instructions.add(`API: ${cyan(`${baseUrl}/api`)}`)
 })
